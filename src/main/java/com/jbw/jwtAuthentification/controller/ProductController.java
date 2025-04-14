cdb5e9e37d31a2eefd440743eedea3597c688649
JONATHAN-BOLIBENGA01 <22bw023@esisalama.org>
2025-04-14 16:52:28 +0200

    fin du crud pour le produit

--- src/main/java/com/jbw/jwtAuthentification/controller/ProductController.java
+++ src/main/java/com/jbw/jwtAuthentification/controller/ProductController.java
@@ -14,15 +14,31 @@ import java.util.List;
 public class ProductController {
     private final ProductService service;
 
+    @PostMapping("/create-product")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        return service.createProduct(product);
+    }
+
     @GetMapping()
     public ResponseEntity<List<Product>> getAllProduct(){
         return service.getProducts();
     }
 
-    @PostMapping("/create-product")
-    public ResponseEntity<Product> createProduct(@RequestBody Product product){
-        return service.createProduct(product);
+    @GetMapping("/getOneProduct/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        return service.getOneProduct(id);
     }
+
+    @PutMapping("/update-product/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
+        return service.updateProduct(id, productDetails);
+    }
+
+    @DeleteMapping("/deleteOneProduct/{ide}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+        return service.deleteProduct(id);
+    }
+
 
 }
 
--- src/main/java/com/jbw/jwtAuthentification/service/ProductService.java
+++ src/main/java/com/jbw/jwtAuthentification/service/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +22,37 @@ public class ProductService {
     public ResponseEntity<Product> createProduct(Product product) {
        Product productcreated = repository.save(product);
        return new ResponseEntity<>(productcreated, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Product> getOneProduct(Long id) {
+        Optional<Product> product = repository.findById(id); //optional parce que le produit peut ou ne pas exister
+        return product.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    public ResponseEntity<Product> updateProduct(Long id, Product productDetails) {
+        Optional<Product> product = repository.findById(id);
+        if (product.isPresent()){
+            Product exixtingProduct = product.get();
+            exixtingProduct.setName(productDetails.getName());
+            exixtingProduct.setPrice(productDetails.getPrice());
+            exixtingProduct.setDescription(productDetails.getDescription());
+
+            Product updateProduct = repository.save(exixtingProduct);
+
+            return  new ResponseEntity<>(updateProduct, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<String> deleteProduct(Long id) {
+        Optional<Product> product = repository.findById(id);
+        if(product.isPresent()){
+            repository.delete(product.get());
+           String msg = "product is deleted";
+           return new ResponseEntity<>(msg, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
